e70dda08cb2c4cb2c759347c5c38fd3ad48a9070
linkjavier <linkjavier@hotmail.com>
2020-05-02 04:23:27 -0500

    Fixed 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{} {}".format(len(argv) - 1,
-                             'arguments:' if (len(argv)) > 2
-                             else 'argument:'))
+                             "arguments:" if (len(argv)) > 2
+                             else "argument:"))
         for i in range(1, len(argv)):
             print("{}: {} ".format(i, argv[i]))

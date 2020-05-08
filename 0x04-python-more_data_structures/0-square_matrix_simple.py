8dc438780ce1c17a88fb4325dc2472ca8fb844f5
linkjavier <linkjavier@hotmail.com>
2020-05-07 23:04:33 -0500

    Added 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in range(0, len(matrix)):
+        new.append(list(map(modpow, matrix[i])))
+    return (new)
+
+def modpow(x):
+    return (x ** 2)

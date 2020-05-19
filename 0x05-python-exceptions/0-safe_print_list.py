d92278babcb53a79c4c0b3a9d9b81e844350e17a
linkjavier <linkjavier@hotmail.com>
2020-05-18 21:03:26 -0500

    Added While

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(0,x):
+        while(i < x):
             print(my_list[i], end="")
+            i = i + 1
+        print()
+        return(i)
     except IndexError:
-        i = i + 1
-    print()
-    return i + 1
+        print()
+        return(i)

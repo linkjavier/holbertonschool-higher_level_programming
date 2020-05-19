3b81a54094fe0d4065d37488ce36941d0045ba4c
linkjavier <linkjavier@hotmail.com>
2020-05-18 20:00:59 -0500

    Added finally to 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,5 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
     except IndexError:
         i = i - 1
-    print()
-    return(i + 1)
+    finally:
+        print()
+        return(i + 1)

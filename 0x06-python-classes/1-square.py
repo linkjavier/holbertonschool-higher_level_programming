138479362d2a510f8097dac3c8c0153255ac8204
linkjavier <linkjavier@hotmail.com>
2020-05-19 20:50:56 -0500

    Added 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """Initialize the object with size value"""
+        self.__size = size

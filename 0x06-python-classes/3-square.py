523c7634b192ad20cbfc4aa0f0db09667709b0bf
linkjavier <linkjavier@hotmail.com>
2020-05-19 21:10:59 -0500

    Added 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write an class Square that defines a square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initialize the object with size value"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Function that returns the area of the Square"""
+        return self.__size*self.__size

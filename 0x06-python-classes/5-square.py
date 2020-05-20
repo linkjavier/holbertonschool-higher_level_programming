3406d7390042b3ba29d0d84e5306201d0aca96a7
linkjavier <linkjavier@hotmail.com>
2020-05-19 23:29:56 -0500

    added 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Write an class Square that defines a square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        """Initialize the object with size value"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Get the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Function that returns the area of the Square"""
+        return self.__size*self.__size
+
+    def my_print(self):
+        """print the square with #"""
+        if self.__size != 0:
+            for i in range(self.__size):
+                print("{}".format("#" * self.__size))
+                i *= 1
+        else:
+            print()

fb88cb73f667db7c031ef95c7cf0601dc5c9de10
linkjavier <linkjavier@hotmail.com>
2020-05-19 21:04:31 -0500

    Fix size=0 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Square Class"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize the object with size value"""
         self.__size = size
         if type(size) is not int:

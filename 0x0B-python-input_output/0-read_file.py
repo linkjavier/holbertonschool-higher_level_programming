1c9309491cd0274e2a46d00f7695c148916a383a
linkjavier <linkjavier@hotmail.com>
2020-06-03 22:51:35 -0500

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" clear
+    Function that reads a text file (UTF8)
+    and prints it to stdout:
+"""
+
+
+def read_file(filename=""):
+    """
+        Function that reads a text file (UTF8)
+        and prints it to stdout:
+    """
+    with open(filename, "encoding=utf8") as a_file:
+        print(a_file.read())

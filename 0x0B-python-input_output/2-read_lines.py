636208240d5a9e27b0445d3a860578da6b8c9e8d
linkjavier <linkjavier@hotmail.com>
2020-06-03 23:57:52 -0500

    Added 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Function that returns the number of lines
+    of a text file:
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+        Function that returns the number of lines
+        of a text file:
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        lines = len(file.readlines())
+        number_lines = len(lines)
+
+        if nb_lines <= 0 or nb_lines > number_lines:
+            for line in lines:
+                print(line, end="")
+        else:
+            number_lines = 1
+            for line in lines:
+                print(line, end="")
+                number_lines += 1
+                if number_lines > nb_lines:
+                    break

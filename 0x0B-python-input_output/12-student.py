dcc67d10b6b271bcc2ff62eacd9711cf2916787a
linkjavier <linkjavier@hotmail.com>
2020-06-05 23:52:07 -0500

    Added 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -22,7 +22,7 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
             Returns the dictionary
         """

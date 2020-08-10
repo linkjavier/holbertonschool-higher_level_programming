4ba1c4379e48cc9707b7bff1a89e5fe3fc19c44e
linkjavier <linkjavier@hotmail.com>
2020-08-09 23:48:56 -0500

    Added 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that takes in arguments and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument. Safe from MySQL injections!
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    dbase = MySQLdb.connect(host="localhost", port=3306,
+                            user=sys.argv[1],
+                            passwd=sys.argv[2],
+                            db=sys.argv[3])
+    cursor = dbase.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id = states.id ORDER BY id ASC;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    dbase.close()

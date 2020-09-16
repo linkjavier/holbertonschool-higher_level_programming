78db0a765a626c0fa1434eca494615e704d6c10c
linkjavier <linkjavier@hotmail.com>
2020-09-16 02:30:18 -0500

    Script that prints the first argument passed to it:

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Script that prints a message depending of the number of arguments passed */
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}

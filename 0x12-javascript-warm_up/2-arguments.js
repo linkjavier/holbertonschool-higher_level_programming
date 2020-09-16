8dfd04cc3e8ae707213e1bc69fe6ce3ccf0daacf
linkjavier <linkjavier@hotmail.com>
2020-09-16 02:06:36 -0500

    Fixed the returned String

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* Script that prints a message depending of the number of arguments passed */
-if ((process.argv.length) === 2) {
-  console.log('No Argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-  console.log('Argument Found');
-} else {
-  console.log('Arguments Found');
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 }

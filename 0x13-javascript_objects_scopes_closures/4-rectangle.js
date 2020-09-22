6b6afb1ab7218d7e702f977189255f493ccf0be9
linkjavier <linkjavier@hotmail.com>
2020-09-21 23:39:55 -0500

    Added all files. Coded until 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/*  Class Rectangle that defines a rectangle with width and height.
+    print(): Prints the rectangle using the character X.
+    rotate(): Exchanges the width and the height of the rectangle.
+    double(): Multiples the width and the height of the rectangle by 2.
+*/
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+};

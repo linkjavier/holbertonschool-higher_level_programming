581a5d9353a73b013b81688ac8f2af4558b457f1
linkjavier <linkjavier@hotmail.com>
2021-03-12 13:58:29 -0500

    Added 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* Script that prints the title of a Star Wars movie where
+    the episode number matches a given integer.
+*/
+const request = require('request');
+const url = process.argv[2];
+const fileName = process.argv[3];
+const fs = require('fs');
+
+request.get(url, function (err, response, body) {
+  if (err == null) {
+    fs.writeFile(fileName, body, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+  }
+});
